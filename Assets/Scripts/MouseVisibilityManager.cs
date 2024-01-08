2f53b5cdbe4c1ad84e5533075073f0ae50af2432
TermiSenpai <escarpaprieto@gmail.com>
2024-01-08 11:46:44 +0100

    Mouse visibility controller

---
+++ Assets/Scripts/MouseVisibilityManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseVisibilityManager : MonoBehaviour
+{
+    public KeyCode Key;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(Key))
+        {
+            switch (Cursor.lockState)
+            {
+                case CursorLockMode.Locked:
+                    Cursor.lockState = CursorLockMode.None;
+                    break;
+                case CursorLockMode.None:
+                    Cursor.lockState = CursorLockMode.Locked;
+                    break;
+            }
+        }
+    }
+}
