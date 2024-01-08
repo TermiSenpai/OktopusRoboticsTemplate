672f4382272c3ae1bddd3bdc9dc37387a9fa9d10
TermiSenpai <escarpaprieto@gmail.com>
2024-01-08 12:46:20 +0100

    spawn and enable machine

--- Assets/Scripts/ActiveMachineManager.cs
+++ Assets/Scripts/ActiveMachineManager.cs
@@ -1,25 +1,39 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ActiveMachineManager : MonoBehaviour
 {
+    [SerializeField] Transform parent;
     [SerializeField] MachineScriptable[] machines;
+    List<GameObject> currentSceneMachines = new List<GameObject>();
     GameObject activeMachine;
     // Start is called before the first frame update
     void Start()
     {
-       activeMachine = machines[0].machineObj;
+        SpawnAllMachinesInScene();
+        changeMachine(currentSceneMachines[0]);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    public void changeMachine(MachineScriptable machine)
+    public void SpawnAllMachinesInScene()
     {
-        activeMachine.SetActive(false);
-        machine.machineObj = activeMachine;
+        foreach (var machine in machines)
+        {
+            currentSceneMachines.Add(Instantiate(machine.machineObj, parent));
+        }
+
+    }
+
+    public void changeMachine(GameObject machine)
+    {
+        if (activeMachine != null)
+            activeMachine.SetActive(false);
+        activeMachine = machine;
         activeMachine.SetActive(true);
     }
 }
