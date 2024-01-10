bbc4199f9aaa08ccdbd0858bc0b8f0b055e070c1
TermiSenpai <escarpaprieto@gmail.com>
2024-01-10 08:19:41 +0100

    Toggle lights with UI btns

--- Assets/Scripts/Lighting/GeneralLightManager.cs
+++ Assets/Scripts/Lighting/GeneralLightManager.cs
@@ -7,6 +7,7 @@ public class GeneralLightManager : MonoBehaviour
     [SerializeField] private Light[] spotLights;
     [SerializeField] ReflectionProbe reflection;
     [SerializeField] private Light pointLight;
+    [SerializeField] private GameObject UI;
 
     [Header("Spot Lights")]
     [SerializeField] Color spotLightsColor = Color.white;
@@ -14,15 +15,48 @@ public class GeneralLightManager : MonoBehaviour
 
     private void Update()
     {
-
         //TODO 
         // Crear evento on modified
         // Seguramente hacer con GUI
+        //foreach (Light light in spotLights)
+        //{
+        //    light.color = spotLightsColor;
+        //    light.intensity = spotIntensity;
+        //    if (spotIntensity < 0) spotIntensity = 0;
+        //}
+    }
+
+    private void OnEnable()
+    {
+        MouseVisibilityManager.MouseRelease += ToggleUI;
+    }
+
+    private void OnDisable()
+    {
+        MouseVisibilityManager.MouseRelease -= ToggleUI;
+        
+    }
+
+    public void OnTogglePointBtn()
+    {
+        pointLight.enabled = !pointLight.enabled;
+    }
+
+    public void OnToggleReflectionBtn()
+    {
+        reflection.enabled = !reflection.enabled;
+    }
+
+    public void OnToggleSpotBtn()
+    {
         foreach (Light light in spotLights)
         {
-            light.color = spotLightsColor;
-            light.intensity = spotIntensity;
-            if (spotIntensity < 0) spotIntensity = 0;
+            light.enabled = !light.enabled;
         }
+    }
+
+    private void ToggleUI()
+    {
+        UI.gameObject.SetActive(!UI.activeInHierarchy);
     }
 }
