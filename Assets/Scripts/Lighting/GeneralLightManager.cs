b9bc81113161c7ec465a25156ae67215cf86c372
TermiSenpai <escarpaprieto@gmail.com>
2024-01-10 10:22:03 +0100

    Better ligths position

--- Assets/Scripts/Lighting/GeneralLightManager.cs
+++ Assets/Scripts/Lighting/GeneralLightManager.cs
@@ -4,14 +4,10 @@ using UnityEngine;
 
 public class GeneralLightManager : MonoBehaviour
 {
+    [SerializeField] private Light MainSpot;
     [SerializeField] private Light[] spotLights;
-    [SerializeField] ReflectionProbe reflection;
     [SerializeField] private Light pointLight;
     [SerializeField] private GameObject UI;
-
-    [Header("Spot Lights")]
-    [SerializeField] Color spotLightsColor = Color.white;
-    [SerializeField] float spotIntensity = 1.0f;
 
     private void Awake()
     {
@@ -33,11 +29,6 @@ public class GeneralLightManager : MonoBehaviour
         pointLight.enabled = !pointLight.enabled;
     }
 
-    public void OnToggleReflectionBtn()
-    {
-        reflection.enabled = !reflection.enabled;
-    }
-
     public void OnToggleSpotBtn()
     {
         foreach (Light light in spotLights)
@@ -48,7 +39,7 @@ public class GeneralLightManager : MonoBehaviour
 
     public void OnSpotIntensitySliderChange(float value)
     {
-        pointLight.intensity = value;       
+        pointLight.intensity = value;
     }
     public void OnPointIntensitySliderChange(float value)
     {
