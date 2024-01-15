d85377e9ba36cdb9109caca10cde8d04a586f377
TermiSenpai <escarpaprieto@gmail.com>
2024-01-15 16:48:50 +0100

    ActiveMachine manager comented

--- Assets/Scripts/ActiveMachineManager.cs
+++ Assets/Scripts/ActiveMachineManager.cs
@@ -4,27 +4,41 @@ using UnityEngine;
 
 public class ActiveMachineManager : MonoBehaviour
 {
+    // Se almacena los datos del scriptable object en un array
     [SerializeField] MachineScriptable[] machines;
 
+    // Se almacena la posición del gameobject padre
     Transform parent;
+
+    // Se almacena una lista variable con los gameobjects para posterior modificación
     List<GameObject> currentSceneMachines = new();
+
+    // Se almacena el gameobject activo
     GameObject activeMachine;
+    // Se almacena el index actual para posterior modificación
     int currentMachineIndex = 0;
-    // Start is called before the first frame update
+
     void Start()
     {
+        // Obtiene el parent de todas las máquinas
         parent = GameObject.FindGameObjectWithTag("MachinesParent").GetComponent<Transform>();
+
+        // Crea las máquinas en escena
         SpawnAllMachinesInScene();
+
+        // Guarda y hace visible la primera máquina
         ChangeMachine(currentSceneMachines[currentMachineIndex]);
     }
 
     public void SpawnAllMachinesInScene()
     {
+        // Todos los gameobjects se almacenan en una lista para poder modificarlo después
         foreach (var machine in machines)
             currentSceneMachines.Add(Instantiate(machine.machineObj, parent));
 
     }
 
+    // Oculta la máquina actual y hace visible una nueva
     public void ChangeMachine(GameObject machine)
     {
         if (activeMachine != null)
@@ -33,6 +47,7 @@ public class ActiveMachineManager : MonoBehaviour
         activeMachine.SetActive(true);
     }
 
+    // Aumenta en 1 el index actual
     public void OnAdvanceMachineBtn()
     {
         int newIndex = currentMachineIndex + 1;
@@ -43,6 +58,7 @@ public class ActiveMachineManager : MonoBehaviour
         ChangeCurrentIndex(newIndex);
     }
 
+    // Disminuye en 1 el index actual
     public void OnDecreaseMachineBtn()
     {
         int newIndex = currentMachineIndex - 1;
@@ -53,6 +69,7 @@ public class ActiveMachineManager : MonoBehaviour
         ChangeCurrentIndex(newIndex);
     }
 
+    // Actualiza la maquina y modifica el index actual
     private void ChangeCurrentIndex(int index)
     {
         ChangeMachine(currentSceneMachines[index]);
