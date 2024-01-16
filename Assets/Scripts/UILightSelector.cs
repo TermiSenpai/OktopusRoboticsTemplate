114820c9f3c257353e8bc118ea6d211f4a5b2f2b
TermiSenpai <escarpaprieto@gmail.com>
2024-01-16 11:30:42 +0100

    UILightSelector comented

--- Assets/Scripts/UILightSelector.cs
+++ Assets/Scripts/UILightSelector.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class UILightSelector : MonoBehaviour
 {
+    // Opciones en el dropdown
     [SerializeField] GameObject[] lightOptions;
+    // Componente transform en canvas
     [SerializeField] RectTransform panel;
-    GameObject currentLightOptions;
-    int currentIndex;
 
-    [SerializeField] float nullHeight;
+    // Opción seleccionada visible
+    GameObject currentLightOptions;
+
+    // Constante del tamaño del contenedor vacio
+    const float NULLHEIGHT = 60f;
 
     private void Start()
     {
+        // La primera opción estará vacia
         currentLightOptions = null;
+
+        // Nos aseguramos de que cualquier menú que nos dejemos activado, se desactive
         foreach (GameObject lightOption in lightOptions)
         {
             if (lightOption != null)
@@ -21,36 +28,43 @@ public class UILightSelector : MonoBehaviour
         }
     }
 
+    // Cuando seleccionas una opción en el dropdown
     public void OnOptionSelected(int value)
     {
         switch (value)
         {
             case 0:
+                // La opción 0 es "Ninguna"
                 ChangeCurrentOptions();
-                currentIndex = value;
                 break;
 
             default:
+                // Cuando se selecciona otra opción
                 ChangeCurrentOptions(lightOptions[value]);
-                currentIndex = value;
                 break;
         }
     }
+    
+    // Sobrecargas
 
     private void ChangeCurrentOptions(GameObject selectedOption)
     {
+        // Comprobación de que no modifique algo que no hay
         if (currentLightOptions != null) currentLightOptions.SetActive(false);
 
+        // La nueva opción se almacena y se hace visible
         currentLightOptions = selectedOption;
         currentLightOptions.SetActive(true);
     }
     private void ChangeCurrentOptions()
     {
+        // Comprobación para que no oculte algo que no hay
         if (currentLightOptions != null) currentLightOptions.SetActive(false);
-        currentLightOptions.SetActive(false);
-        panel.sizeDelta = new Vector2(panel.sizeDelta.x, nullHeight);
+
+        // Se modifica el tamaño del contenedor a uno ajustado 
+        panel.sizeDelta = new Vector2(panel.sizeDelta.x, NULLHEIGHT);
+
+        // Nos aseguramos de que no haya ningun menú activo
         currentLightOptions = null;
     }
-
-
 }
