a0b3426bf0737fc96ee340f7ca6adc32168c3728
TermiSenpai <escarpaprieto@gmail.com>
2024-01-17 09:56:48 +0100

    LighRotation comented

--- Assets/Scripts/Lighting/LightRotation.cs
+++ Assets/Scripts/Lighting/LightRotation.cs
@@ -1,11 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class LightRotation : MonoBehaviour
 {
-    public void OnRotateSlider(float value)
-    {
-        transform.rotation = Quaternion.Euler(0, value, 0);
-    }
+    // Se modifica la rotación en el eje Y del gameobject actual a través de un slider
+    public void OnRotateSlider(float value) => transform.rotation = Quaternion.Euler(0, value, 0);
 }
