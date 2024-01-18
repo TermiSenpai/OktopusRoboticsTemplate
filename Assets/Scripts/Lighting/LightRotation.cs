7a9f7b2c67e797cd38ad12d2b5582bb6f69f2e64
TermiSenpai <escarpaprieto@gmail.com>
2024-01-18 08:17:24 +0100

    light rotation slider value to txt

--- Assets/Scripts/Lighting/LightRotation.cs
+++ Assets/Scripts/Lighting/LightRotation.cs
@@ -1,7 +1,14 @@
+using TMPro;
 using UnityEngine;
 
 public class LightRotation : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI txtValue;
+
     // Se modifica la rotación en el eje Y del gameobject actual a través de un slider
-    public void OnRotateSlider(float value) => transform.rotation = Quaternion.Euler(0, value, 0);
+    public void OnRotateSlider(float value)
+    {
+        transform.rotation = Quaternion.Euler(0, value, 0);
+        txtValue.text = value.ToString("0.0");
+    }
 }
