f22396dedc8249f5a42544eeda4e41f4cb7485bf
TermiSenpai <escarpaprieto@gmail.com>
2024-01-23 09:32:26 +0100

    Sliders controller

--- Assets/Scripts/Simulation/PaletizerAxisMovement.cs
+++ Assets/Scripts/Simulation/PaletizerAxisMovement.cs
@@ -2,29 +2,48 @@ using UnityEngine;
 
 public class PaletizerAxisMovement : MonoBehaviour
 {
+    public static PaletizerAxisMovement Instance;
+
     Transform xAxis;
     Transform yAxis;
     Transform zAxis;
 
-    private void Start()
+    private void Awake()
     {
+        Instance = this;
         xAxis = GameObject.FindGameObjectWithTag("X").transform;
         yAxis = GameObject.FindGameObjectWithTag("Y").transform;
         zAxis = GameObject.FindGameObjectWithTag("Z").transform;
     }
 
+    private void Start()
+    {
+    }
+
     public void OnSliderX(float value)
     {
-        xAxis.position = new Vector3(value, xAxis.position.y, xAxis.position.z);
+        xAxis.localPosition = new Vector3(value, xAxis.localPosition.y, xAxis.localPosition.z);
     }
     public void OnSliderY(float value)
     {
-        yAxis.position = new Vector3(yAxis.position.x, value, yAxis.position.z);
+        yAxis.localPosition = new Vector3(yAxis.localPosition.x, value, yAxis.localPosition.z);
     }
     public void OnSliderZ(float value)
     {
-        xAxis.position = new Vector3(zAxis.position.x, zAxis.position.y, value);
+        zAxis.localPosition = new Vector3(zAxis.localPosition.x, zAxis.localPosition.y, value);
     }
 
+    public float GetAxisX()
+    {
+        return xAxis.localPosition.x;
+    }
+    public float GetAxisY()
+    {
+        return yAxis.localPosition.y;
+    }
+    public float GetAxisZ()
+    {
+        return zAxis.localPosition.z;
+    }
 
 }
