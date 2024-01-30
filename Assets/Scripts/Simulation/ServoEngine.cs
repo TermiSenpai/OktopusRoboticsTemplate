b7e7a5af97b55d944a136d6c77e2a4bb62dd6bcf
TermiSenpai <escarpaprieto@gmail.com>
2024-01-30 10:08:32 +0100

    Debug Engine logic

--- Assets/Scripts/Simulation/ServoEngine.cs
+++ Assets/Scripts/Simulation/ServoEngine.cs
@@ -8,12 +8,26 @@ public class ServoEngine : MonoBehaviour
 
     [SerializeField] Vector3 direction;
     [SerializeField] GameObject axis;
-
+    [SerializeField] bool debugR;
+    [SerializeField] bool debugL;
     private void Update()
     {
-        if (!PLCConexion.plc.IsConnected) return;
+
+        // Debug
+        if (debugR)
+            axis.transform.localPosition += direction * speed;
+
+        if (debugL)
+            axis.transform.localPosition -= direction * speed;
+
+        if (PLCConexion.plc == null || !PLCConexion.plc.IsConnected) return;
 
        bool rightMove = (bool) PLCConexion.plc.Read(rightCode);
+        if(rightMove)
+        {
+            axis.transform.localPosition += direction * speed;
+        }
+       bool leftMove = (bool) PLCConexion.plc.Read(leftCode);
         if(rightMove)
         {
             axis.transform.localPosition += direction * speed;
