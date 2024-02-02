89fe39bb2fd1e4f305eac368895771e6a991efea
TermiSenpai <escarpaprieto@gmail.com>
2024-02-02 11:00:13 +0100

    Conveyor commented

--- Assets/Scripts/Simulation/ConveyorBelt.cs
+++ Assets/Scripts/Simulation/ConveyorBelt.cs
@@ -1,28 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ConveyorBelt : MonoBehaviour
 {
+    // Variable que almacena la velocidad actual de la cinta transportadora
     [SerializeField] private float currentSpeed;
+
+    // Método para establecer la velocidad de la cinta transportadora
     public void SetSpeed(float speed)
     {
         currentSpeed = speed;
     }
 
+    // Método para obtener la velocidad actual de la cinta transportadora
     public float GetSpeed()
     {
         return currentSpeed;
     }
 
+    // Método para obtener el estado del sensor y ajustar la velocidad en consecuencia
     public void GetSensorState(bool sensorState)
     {
+        // Estructura de control switch para manejar el estado del sensor
         switch (sensorState)
         {
+            // Si el estado del sensor es true, detener la cinta (velocidad = 0)
             case true:
                 SetSpeed(0f);
-                break; 
-            
+                break;
+
+            // Si el estado del sensor es false, establecer una velocidad predeterminada (0.5)
             case false:
                 SetSpeed(0.5f);
                 break;
