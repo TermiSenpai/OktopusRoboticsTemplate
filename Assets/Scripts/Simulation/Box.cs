07312e6ad57cc6fb5405d7f10fdc59154dd94a36
TermiSenpai <escarpaprieto@gmail.com>
2024-02-02 08:15:48 +0100

    Belt speed

--- Assets/Scripts/Simulation/Box.cs
+++ Assets/Scripts/Simulation/Box.cs
@@ -4,9 +4,28 @@ using UnityEngine;
 
 public class Box : MonoBehaviour
 {
+    [SerializeField] private float currentSpeed = 0f;
+
+
     private void OnCollisionEnter(Collision collision)
     {
-        
+        if (collision.collider.CompareTag("Belt"))
+        {
 
+            collision.gameObject.TryGetComponent<ConveyorBelt>(out var belt);
+            currentSpeed = belt.GetSpeed();
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.collider.CompareTag("Belt"))
+        {
+            collision.gameObject.TryGetComponent<ConveyorBelt>(out var belt);
+            if (currentSpeed == belt.GetSpeed())
+                return;
+
+            currentSpeed = belt.GetSpeed();
+        }
     }
 }
--- Assets/Scripts/Simulation/ConveyorBelt.cs
+++ Assets/Scripts/Simulation/ConveyorBelt.cs
@@ -4,5 +4,15 @@ using UnityEngine;
 
 public class ConveyorBelt : MonoBehaviour
 {
-    
+    private float currentSpeed;
+
+    public void SetSpeed(float speed)
+    {
+        currentSpeed = speed;
+    }
+
+    public float GetSpeed()
+    {
+        return currentSpeed;
+    }
 }
