d6b32a70103cd276dfdd2bed47488a42a8f64dd9
TermiSenpai <escarpaprieto@gmail.com>
2024-03-07 09:47:07 +0100

    Added more sequences and time calculator

--- Assets/Scripts/DisconnectedPLCAutoMovements/AutoMovementManager.cs
+++ Assets/Scripts/DisconnectedPLCAutoMovements/AutoMovementManager.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using Unity.Collections;
 using UnityEngine;
 
 public class AutoMovementManager : MonoBehaviour
@@ -16,13 +17,38 @@ public class AutoMovementManager : MonoBehaviour
     [SerializeField] private Vector3[] upperPallet; // Array to store pallet positions
     [SerializeField] int index;
 
-    private void Awake() => box = FindObjectOfType<BoxManager>(); // Finding the BoxManager component in the scene and assigning it to the box variable
+    [SerializeField] private float sequenceTime;
+    [SerializeField] private float totalSequenceTime;
 
-    public void StartPaletizer() => StartCoroutine(StartSequence()); // Initiating the StartSequence coroutine
+    [SerializeField] private float sequencesPerMinute;
+
+    private void Awake() => box = FindObjectOfType<BoxManager>(); // Finding the BoxManager component in the scene and assigning it to the box variable
+    private void Start()
+    {
+        // Initialize upperPallet array with the same length as palletPositions
+        upperPallet = new Vector3[palletPositions.Length];
+
+        // Fill upperPallet array with positions having Y coordinate of 1
+        for (int i = 0; i < palletPositions.Length; i++)
+        {
+            upperPallet[i] = new Vector3(palletPositions[i].x, 1f, palletPositions[i].z);
+        }
+    }
+    private void Update()
+    {
+        sequenceTime += Time.deltaTime;
+        totalSequenceTime += Time.deltaTime;
+    }
+    public void StartPaletizer()
+    {
+        totalSequenceTime = 0;
+        StartCoroutine(StartSequence()); // Initiating the StartSequence coroutine
+    }
 
     IEnumerator StartSequence()
     {
-        ChangeSpeed(0.005f);
+        sequenceTime = 0;
+        ChangeSpeed(0.007f);
         // Go to belt
         yield return StartCoroutine(MoveToTarget(beltTarget)); // Initiating a coroutine to move to the belt position
 
@@ -31,19 +57,21 @@ public class AutoMovementManager : MonoBehaviour
 
         // Elevate axis
         yield return StartCoroutine(MoveToTarget(takeBoxAndElevate)); // Initiating a coroutine to move to the position to take and elevate the box
+        yield return StartCoroutine(MoveToTarget(upperPallet[index]));
 
         ChangeSpeed(0.003f);
         // Go to pallet
-        yield return StartCoroutine(MoveToTarget(upperPallet[index]));
         yield return StartCoroutine(MoveToTarget(palletPositions[index])); // Initiating a coroutine to move to the current pallet position
 
         // Drop box
         box.OnDropBtn(); // Calling the OnDropBtn method of the box
 
-        ChangeSpeed(0.005f);
+        ChangeSpeed(0.007f);
         // Elevate axis
         yield return StartCoroutine(MoveToTarget(upperPallet[index]));
         //yield return StartCoroutine(MoveToTarget(takeBoxAndElevate)); // Initiating a coroutine to move to the position to elevate the box again
+
+        CalculateTime();
 
         // Repeat
         if (index < palletPositions.Length - 1) // Checking if there are more pallet positions to visit
@@ -56,6 +84,7 @@ public class AutoMovementManager : MonoBehaviour
 
     IEnumerator MoveToTarget(Vector3 target)
     {
+        Debug.Log($"Going to {target}");
         while (true)
         {
             // Checking if each axis is at the target position within a small threshold
@@ -87,4 +116,6 @@ public class AutoMovementManager : MonoBehaviour
         axisY.speed = value;
         axisZ.speed = value;
     }
+
+    void CalculateTime() => sequencesPerMinute = index / (totalSequenceTime / 60f);
 }
